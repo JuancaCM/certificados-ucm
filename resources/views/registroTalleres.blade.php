fa0989d6642cb2a4ed35f79763f815aedbf7f5c9
Juan <juancarlos.ca.mo@gmail.com>
2022-10-27 18:47:41 -0300

    Vista registro dimensiones y talleres

---
+++ resources/views/registroTalleres.blade.php
@@ -0,0 +1,15 @@
+@extends("base")
+
+@section("content")
+<div class="card mt-3 col-md-4 offset-md-4 border-left-primary shadow">
+    <div class="card-body" >
+        
+        
+        <button type="button" class="btn btn-primary mt-3">Enviar</button>
+
+
+
+    </div>
+</div>
+
+@endsection
