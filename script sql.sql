6c2fe54aeec267ecd20e315ed001a0940243f101
Luis Varela <luxo_v7@hotmail.com>
2022-10-27 19:33:06 -0300

    Creado script para pruebas

---
+++ script sql.sql
@@ -0,0 +1,18 @@
+INSERT INTO roles (name) VALUES ('Administrador');
+INSERT INTO roles (name) VALUES ('Docente');
+INSERT INTO roles (name) VALUES ('Analista');
+
+INSERT INTO faculties (name) VALUES ('Ingenieria');
+INSERT INTO faculties (name) VALUES ('Salud');
+INSERT INTO faculties (name) VALUES ('Educacion');
+
+INSERT INTO careers (name) VALUES ('Informatica');
+INSERT INTO careers (name) VALUES ('Kinesiologia');
+INSERT INTO careers (name) VALUES ('Pedagogia');
+
+INSERT INTO contracts (name) VALUES ('Planta Ordinario');
+INSERT INTO contracts (name) VALUES ('Planta Asociado');
+INSERT INTO contracts (name) VALUES ('Planta Administrativo');
+
+INSERT INTO campuses (name) VALUES ('Sede Talca');
+INSERT INTO campuses (name) VALUES ('Sede Curico');
