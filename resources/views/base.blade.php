271ba1bf558327ad04df56410a1c08af40d49c4c
Luis Varela <luxo_v7@hotmail.com>
2022-11-08 21:33:18 -0300

    Cambios visuales menores

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -41,52 +41,44 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="/registro">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Registro</span></a>
-            </li>
 
-            <li class="nav-item active">
+
+            {{-- <li class="nav-item active">
                 <a class="nav-link" href="/login">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Login</span></a>
-            </li>
+            </li> --}}
 
             <li class="nav-item active">
-                <a class="nav-link" href="/perfil">
+                <a class="nav-link" href="/registro">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Perfil</span></a>
-            </li>
-
-            <li class="nav-item active">
-                <a class="nav-link" href="/contacto">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Contacto</span></a>
+                    <span>Usuarios</span></a>
             </li>
 
             <li class="nav-item active">
                 <a class="nav-link" href="/registroCertificaciones">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Registro certificaciones</span></a>
+                    <span>Certificaciones</span></a>
             </li>
 
             <li class="nav-item active">
                 <a class="nav-link" href="/registroDimensiones">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Registro dimensiones</span></a>
+                    <span>Dimensiones</span></a>
             </li>
+
+
 
             <!-- Divider -->
             <hr class="sidebar-divider">
 
             <!-- Heading -->
-            <div class="sidebar-heading">
+            {{-- <div class="sidebar-heading">
                 Interface
-            </div>
+            </div> --}}
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
@@ -99,10 +91,10 @@
                         <a class="collapse-item" href="cards.html">Cards</a>
                     </div>
                 </div>
-            </li>
+            </li> --}}
 
             <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
@@ -118,18 +110,18 @@
                         <a class="collapse-item" href="utilities-other.html">Other</a>
                     </div>
                 </div>
-            </li>
+            </li> --}}
 
             <!-- Divider -->
-            <hr class="sidebar-divider">
+            {{-- <hr class="sidebar-divider"> --}}
 
             <!-- Heading -->
-            <div class="sidebar-heading">
+            {{-- <div class="sidebar-heading">
                 Addons
-            </div>
+            </div> --}}
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-fw fa-folder"></i>
@@ -147,24 +139,24 @@
                         <a class="collapse-item" href="blank.html">Blank Page</a>
                     </div>
                 </div>
-            </li>
+            </li> --}}
 
             <!-- Nav Item - Charts -->
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="charts.html">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Charts</span></a>
-            </li>
+            </li> --}}
 
             <!-- Nav Item - Tables -->
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="tables.html">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Tables</span></a>
-            </li>
+            </li> --}}
 
             <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+            {{-- <hr class="sidebar-divider d-none d-md-block"> --}}
 
 
 
@@ -347,21 +339,13 @@
                                     aria-labelledby="userDropdown">
                                     <a class="dropdown-item" href="#">
                                         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Profile
-                                    </a>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Settings
-                                    </a>
-                                    <a class="dropdown-item" href="#">
-                                        <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Activity Log
+                                        Perfil
                                     </a>
                                     <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="#" data-toggle="modal"
                                         data-target="#logoutModal">
                                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Logout
+                                        Cerrar Sesión
                                     </a>
                                 </div>
                             </li>
