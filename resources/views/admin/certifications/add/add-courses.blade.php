2a6ee09b5c05b955b357aa35774e27480aa2e9cc
Luis Varela <luxo_v7@hotmail.com>
2022-12-30 18:20:17 -0300

    Inicio cambios a pagina

--- resources/views/admin/certifications/add/add-courses.blade.php
+++ resources/views/admin/certifications/add/add-courses.blade.php
@@ -7,11 +7,11 @@
                 <div class="col s12 m6 mt-3">
                     @if (null != session('insert') && session('insert'))
                         <div class="alert alert-success text-center">
-                            Taller registrado correctamente en la base de datos
+                            Registrado correctamente en la base de datos
                         </div>
                     @elseif (null != session('insert') && !session('insert'))
                         <div class="alert alert-danger text-center">
-                            Ha ocurrido un error al registrar el taller
+                            Ha ocurrido un error al registrar
                         </div>
                     @endif
                 </div>
@@ -26,12 +26,17 @@
                                 <label><span style="color: red">*</span>Nombre de la certificación: <i
                                         class="bi bi-question-circle" data-toggle="tooltip" data-placement="right"
                                         title="Si no encuentra la certificación, vaya a la sección de registro de certificaciones."></i></label>
-                                <select name="name" class="form-control mb-3" required>
-                                    <option selected disabled>Certificación</option>
-                                    @foreach ($course_names as $course_name)
-                                        <option value="{{ $course_name->id }}">{{ $course_name->name }}</option>
-                                    @endforeach
-                                </select>
+                                <div class="input-group">
+                                    <select name="name" class="form-control mb-3" required>
+                                        <option selected disabled>Certificación</option>
+                                        @foreach ($course_names as $course_name)
+                                            <option value="{{ $course_name->id }}">{{ $course_name->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <a href="#" class="btn btn-primary btn-sm ml-1 mb-3" role="button">
+                                        <i class="fa-solid fa-plus mt-2"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
@@ -39,23 +44,33 @@
                                 <label><span style="color: red">*</span>Público objetivo: <i class="bi bi-question-circle"
                                         data-toggle="tooltip" data-placement="right"
                                         title="Si no encuentra el publico objetivo, vaya a la sección de registro de publico objetivo."></i></label>
-                                <select name="target_audience" class="form-control mb-3" required>
-                                    <option selected disabled>Publico objetivo</option>
-                                    @foreach ($target_audiences as $target_audience)
-                                        <option value="{{ $target_audience->id }}">{{ $target_audience->name }}</option>
-                                    @endforeach
-                                </select>
+                                <div class="input-group">
+                                    <select name="target_audience" class="form-control mb-3" required>
+                                        <option selected disabled>Publico objetivo</option>
+                                        @foreach ($target_audiences as $target_audience)
+                                            <option value="{{ $target_audience->id }}">{{ $target_audience->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <a href="/nuevo-publico-objetivo" class="btn btn-primary btn-sm ml-1 mb-3" role="button">
+                                        <i class="fa-solid fa-plus mt-2"></i>
+                                    </a>
+                                </div>
                             </div>
                             <div class="col s12 m6">
                                 <label><span style="color: red">*</span>Sede: <i class="bi bi-question-circle"
                                         data-toggle="tooltip" data-placement="right"
                                         title="Si no encuentra la sede, vaya a la sección de registro de sedes."></i></label>
-                                <select name="campus" class="form-control mb-3" required>
-                                    <option selected disabled>Sede</option>
-                                    @foreach ($campuses as $campus)
-                                        <option value="{{ $campus->id }}">{{ $campus->name }}</option>
-                                    @endforeach
-                                </select>
+                                <div class="input-group">
+                                    <select name="campus" class="form-control mb-3" required>
+                                        <option selected disabled>Sede</option>
+                                        @foreach ($campuses as $campus)
+                                            <option value="{{ $campus->id }}">{{ $campus->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <a href="#" class="btn btn-primary btn-sm ml-1 mb-3" role="button">
+                                        <i class="fa-solid fa-plus mt-2"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
@@ -63,12 +78,17 @@
                                 <label><span style="color: red">*</span>Tipo de certificación: <i
                                         class="bi bi-question-circle" data-toggle="tooltip" data-placement="right"
                                         title="Si no encuentra el tipo de certificación, vaya a la sección de registro de tipos de certificación."></i></label>
-                                <select name="type" class="form-control mb-3" required>
-                                    <option selected disabled>Tipo</option>
-                                    @foreach ($types as $type)
-                                        <option value="{{ $type->id }}">{{ $type->name }}</option>
-                                    @endforeach
-                                </select>
+                                <div class="input-group">
+                                    <select name="type" class="form-control mb-3" required>
+                                        <option selected disabled>Tipo</option>
+                                        @foreach ($types as $type)
+                                            <option value="{{ $type->id }}">{{ $type->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <a href="#" class="btn btn-primary btn-sm ml-1 mb-3" role="button">
+                                        <i class="fa-solid fa-plus mt-2"></i>
+                                    </a>
+                                </div>
                             </div>
                             <div class="col s12 m6">
                                 <label><span style="color: red">*</span>Estado del curso: <i class="bi bi-question-circle"
@@ -114,7 +134,8 @@
                         <div class="row">
                             <div class="col s12 m6">
                                 <label><span style="color: red">*</span>Horas sincrónicas:</label>
-                                <input name="synchronous_hours" type="number" min="0" class="form-control" required>
+                                <input name="synchronous_hours" type="number" min="0" class="form-control"
+                                    required>
                             </div>
                             <div class="col s12 m6">
                                 <div class="form-label mb-3">
@@ -139,12 +160,18 @@
                                 <label><span style="color: red">*</span>Relator del curso: <i
                                         class="bi bi-question-circle" data-toggle="tooltip" data-placement="right"
                                         title="Si no encuentra al relator, vaya a la sección de registro de relatores."></i></label>
-                                <select name="course_teacher" class="form-control mb-3" required>
-                                    <option selected disabled>Relator</option>
-                                    @foreach ($course_teachers as $course_teacher)
-                                        <option value="{{ $course_teacher->id }}">{{ $course_teacher->name }}</option>
-                                    @endforeach
-                                </select>
+                                <div class="input-group">
+                                    <select name="course_teacher" class="form-control mb-3" required>
+                                        <option selected disabled>Relator</option>
+                                        @foreach ($course_teachers as $course_teacher)
+                                            <option value="{{ $course_teacher->id }}">{{ $course_teacher->name }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    <a href="#" class="btn btn-primary btn-sm ml-1 mb-3" role="button">
+                                        <i class="fa-solid fa-plus mt-2"></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
